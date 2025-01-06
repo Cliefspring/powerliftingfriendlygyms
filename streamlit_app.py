b2d418e5c683c99e603e4b49eea450c6e7382b4e
Andrey Vazhentsev <vazhentsev@inbox.ru>
2025-01-07 02:19:03 +0300

    connected google sheets

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,14 @@ st.write(
 st.write(
     "Add another text here!"
 )
+
+from streamlit_gsheets import GSheetsConnection
+
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+df = conn.read()
+
+
+for row in df.iloc[:1].itertuples():
+    st.write(f"Gym '{row.Gym}' is located in country '{row.Country}'")
