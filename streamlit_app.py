ff7188dd417a15207e7cee40b41e72f0a974d1c6
Andrey Vazhentsev <vazhentsev@inbox.ru>
2025-01-12 22:14:12 +0300

    added follium map

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
+import folium
+
 import streamlit as st
+
+from streamlit_folium import st_folium
+from streamlit_gsheets import GSheetsConnection
 
 st.title("🎈 My new app")
 st.write(
@@ -8,13 +13,21 @@ st.write(
     "Add another text here!"
 )
 
-from streamlit_gsheets import GSheetsConnection
-
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 df = conn.read()
 
-
 for row in df.iloc[:1].itertuples():
     st.write(f"Gym '{row.Gym}' is located in country '{row.Country}'")
+
+
+m = folium.Map(location=[df.iloc[:1]['latitude'], df.iloc[:1]['longtitude']], zoom_start=16)
+for index, row in df.iloc[:1].iterrows(): 
+    folium.Marker(
+        [row['latitude'], row['longtitude']], 
+        tooltip=row['Gym'],
+    ).add_to(m)
+
+# call to render Folium map in Streamlit
+st_data = st_folium(m, width=725)
