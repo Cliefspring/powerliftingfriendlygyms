3ab16c65bcfb7f9de363d036a3735aebd211df83
Andrey Vazhentsev <vazhentsev@inbox.ru>
2025-01-13 00:16:13 +0300

    improved the app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,109 @@
 import folium
 
 import streamlit as st
+import pandas as pd
 
 from streamlit_folium import st_folium
 from streamlit_gsheets import GSheetsConnection
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.set_page_config(
+    page_title="Powerlift Anywhere", 
+    page_icon=None, 
+    layout="wide", 
+    initial_sidebar_state="auto", 
+    menu_items=None,
 )
-st.write(
-    "Add another text here!"
-)
+
+
+st.title("Powerlifting Gyms Around the World")
+st.write("Training while traveling is easy. Choose country and city to explore powerlifting gyms there")
+
+COLUMN_WEBSITE = 'website'
+COLUMN_PHONE = 'phone'
+COLUMN_GOOGLE_MAP_LINK = 'Google Map Link'
+
+df = pd.read_csv('data/gyms_powerlifting.csv', converters={COLUMN_PHONE : str})
 
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
-
 df = conn.read()
 
-for row in df.iloc[:1].itertuples():
-    st.write(f"Gym '{row.Gym}' is located in country '{row.Country}'")
+COLUMNS_TO_SHOW = [
+    'Gym', 'Squat rack', 'Weight Bench', 
+    COLUMN_GOOGLE_MAP_LINK,
+    COLUMN_WEBSITE,
+    COLUMN_PHONE,
+    'Country', 'City', 'Address', 
+]
+
+df = df.dropna(subset=['Gym'])
+
+COUNTRIES = sorted(df['Country'].unique().tolist())
+
+OPTIONS_COUNTRIES = st.multiselect(
+    label="Choose country",
+    options=COUNTRIES,
+    default=[],
+)
+
+if len(OPTIONS_COUNTRIES) > 0:
+    df = df[df['Country'].isin(OPTIONS_COUNTRIES)]
+
+CITIES = sorted(df['City'].unique().tolist())
+
+OPTIONS_CITIES = st.multiselect(
+    label="Choose city",
+    options=CITIES,
+    default=[],
+)
+
+if len(OPTIONS_CITIES) > 0:
+    df = df[df['City'].isin(OPTIONS_CITIES)]
+
+N_GYMS_FOUND = len(df)
+st.text(f'{N_GYMS_FOUND} gym{"s" if N_GYMS_FOUND > 1 else ""} found')
+
+st.dataframe(
+    df[COLUMNS_TO_SHOW], 
+    hide_index=True, 
+    width=1200,
+    use_container_width=False,
+    column_config={
+        COLUMN_GOOGLE_MAP_LINK: st.column_config.LinkColumn(
+            label=COLUMN_GOOGLE_MAP_LINK, width='medium'),
+        COLUMN_WEBSITE: st.column_config.LinkColumn(
+            label=COLUMN_WEBSITE, width='medium'),
+    }
+)
+
+COLUMN_LATITUDE = 'latitude'
+COLUMN_LONGITUDE = 'longtitude'
 
 
-m = folium.Map(location=[df.iloc[:1]['latitude'], df.iloc[:1]['longtitude']], zoom_start=16)
-for index, row in df.iloc[:1].iterrows(): 
+
+m = folium.Map()
+
+def get_tooltip_text(row):
+    if str(row['website']).startswith('http'):
+        return f"""{row['Gym']}<br><a href=\"{row['website']}\">
+{row['website']}</a><br>{row['phone']}<br>{row['Address']}"""
+    else:
+        return f"""{row['Gym']}<br>{row['phone']}<br>{row['Address']}"""
+
+
+LATITUDES = df[COLUMN_LATITUDE].tolist()
+LONGITUDES = df[COLUMN_LONGITUDE].tolist()
+TOOLTIPS = df.apply(
+    lambda row: get_tooltip_text(row), 
+    axis=1).tolist()
+
+for LATITUDE, LONGITUDE, TOOLTIP in zip(LATITUDES, LONGITUDES, TOOLTIPS): 
     folium.Marker(
-        [row['latitude'], row['longtitude']], 
-        tooltip=row['Gym'],
+        [LATITUDE, LONGITUDE], 
+        tooltip=TOOLTIP,
+        popup=TOOLTIP,
+        auto_close=False,
     ).add_to(m)
 
 # call to render Folium map in Streamlit
-st_data = st_folium(m, width=725)
+st_data = st_folium(m, width=1000)
